ef728349758d420571cbaaa9c26d6d380adc21b3
ashish-irdeto <mythrir@hcl.com>
2019-10-21 19:45:35 +0530

    merge conflicts

--- src/main/java/com/insurance/hcis/controller/LoginController.java
+++ src/main/java/com/insurance/hcis/controller/LoginController.java
@@ -1,5 +1,50 @@
 package com.insurance.hcis.controller;
 
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.insurance.hcis.dto.RequestApproverDto;
+import com.insurance.hcis.dto.ResponseApproverDto;
+import com.insurance.hcis.exception.CommonException;
+import com.insurance.hcis.service.LoginService;
+import com.insurance.hcis.util.ApplicationConstants;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author SubhaMaheswaran
+ * @Description This class is used to do the login operation
+ */
+@RestController
+@CrossOrigin(allowedHeaders = { "*", "*/" }, origins = { "*", "*/" })
+@RequestMapping("/approver")
+@Slf4j
 public class LoginController {
 
+	@Autowired
+	LoginService loginService;
+
+	/** 
+	 * @Description This method is used for approver login
+	 * @param RequestApproverDto
+	 * @return ResponseApproverDto
+	 * @exception INVALID_CREDENTIALS
+	 */
+	@PostMapping("/login")
+	public Optional<ResponseApproverDto> login(@RequestBody RequestApproverDto requestApproverDto)
+			throws CommonException {
+		log.info(":: Enter into LoginController--------::login()");
+		Optional<ResponseApproverDto> responseApproverDto = loginService.login(requestApproverDto);
+		if (!(responseApproverDto.isPresent())) {
+			throw new CommonException(ApplicationConstants.INVALID_CREDENTIALS);
+		}
+		responseApproverDto.get().setMessage("login successfull");
+		responseApproverDto.get().setStatusCode(200);
+		return responseApproverDto;
+	}
 }
