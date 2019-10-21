c9cc74cbec1cbad5b6e0856d354fd899cc7a081b
AVK Kiran Kumar <avk.kirankumar@gmail.com>
2019-10-21 15:52:27 +0530

    project setup

---
+++ src/main/java/com/insurance/hics/HealthinsuranceclaimsystemApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.hics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthinsuranceclaimsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthinsuranceclaimsystemApplication.class, args);
+	}
+
+}
