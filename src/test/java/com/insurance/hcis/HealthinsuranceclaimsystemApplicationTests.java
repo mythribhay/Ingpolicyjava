632038c904eb34e0ca965ee4b91a1d6524d15b58
ashish-irdeto <mythrir@hcl.com>
2019-10-22 11:50:06 +0530

    test cases

--- src/test/java/com/insurance/hcis/HealthinsuranceclaimsystemApplicationTests.java
+++ src/test/java/com/insurance/hcis/HealthinsuranceclaimsystemApplicationTests.java
@@ -1,17 +1,8 @@
 package com.insurance.hcis;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> ba760430c8145e4ee2d07e1aa36ed1a0b0e2a883
-import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HealthinsuranceclaimsystemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
